62380ebc16700bee2b0212a50b1a91a49ae4a867
miguelcebo <55684795+miguelcebo@users.noreply.github.com>
2020-02-19 09:36:53 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Invitación Boda
+
+Aplicación de invitación boda.
+
+El objetivo es desarrollar una aplicación para controlar las invitaciones a una boda, que tendrá un aspecto final como el de la imagen
+
+![AppBoda](images/appBoda.png)
+
+Puedes moficiar el contexto a otro que quieras, un partido de futbol, una lan party para jugar partidas en red con amigos, etc. 
+pero la funcionad pedida será la misma y debes respetar los nombres de las clases css
+
+La estructura básica html y css ya está creada, pero eres libre para modificarla si así lo deseas.
+
+Completa todas las historias utilizando DOM
+
+1) Cómo Administrador del evento, querría añadir nombres a la lista de invitados para saber quien asistirá.
+1) Cómo Administrador del evento, querría poder confirmar invitados al evento para saber quien finalmente irá
+1) Cómo Administrador del evento poder editar el nombre de cada asistente al evento para corregir errores
+1) Cómo Administrador del evento querría poder filtrar los invitados para mostrar solo los que tienen confirmada la asistencia para visualizarlo mejor
+1) Cómo Administrador del evento, querría poder eliminar invitados al evento que se hayan dado de baja
+1) Cómo Administrador del evento me confirme antes de borrar un invitado al evento para evitar cometer un error
+1) Cómo Administrador del evento querría que no se pudiera repetir el nombre de un invitado al evento ni añadir nombres vacios para evitar errores
+
---
+++ js/dom.js
@@ -0,0 +1,155 @@
+preestablecidos();
+document.getElementById("ocultar").addEventListener('change', ocultarmostrar);
+document.getElementById("enviar").addEventListener("click", añadirInvitado);
+
+
+function preestablecidos(){
+
+    let borrar1 = document.getElementsByClassName("eliminar")[0];
+    let borrar2 = document.getElementsByClassName("eliminar")[1];
+
+    borrar1.addEventListener("click", function(){
+        borrar(borrar1);
+    }); 
+    borrar2.addEventListener("click", function(){
+        borrar(borrar2);
+    });
+
+    confirmado(document.getElementById("1"));
+    confirmado(document.getElementById("2"));
+
+    document.getElementsByTagName("button")[1].addEventListener("click", function(){
+        editar(document.getElementsByTagName("span")[0], document.getElementsByTagName("li")[0]);
+    });
+    document.getElementsByTagName("button")[3].addEventListener("click", function(){
+        editar(document.getElementsByTagName("span")[1], document.getElementsByTagName("li")[1]);
+    });
+
+}
+
+function ocultarmostrar(){
+
+    let x = document.getElementsByClassName("noconfirmed");
+
+    for(let i = 0; i < x.length; i++){
+
+        if(document.getElementById("ocultar").checked){
+            document.getElementsByClassName("noconfirmed")[i].style.display = "none";
+        }else{
+            document.getElementsByClassName("noconfirmed")[i].style.display = "block";
+        } 
+
+    }
+
+}
+
+
+
+function norep(nombre){
+
+    let x = document.getElementsByTagName("span");
+
+    event.preventDefault();
+    if(nombre != ""){
+        
+        for(i = 0; i<x.length; i++){
+
+            if(x[i].innerHTML == nombre){
+                return false;
+            }
+    
+        }
+        
+        return true;
+    }
+    return false;
+
+}
+
+    
+function editar(span, padre){
+
+    let x = span.innerHTML;
+    var crear = document.createElement("input");
+
+    crear.setAttribute("type", "text");
+    padre.replaceChild(crear, span);
+    crear.value = x;
+    crear.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+         x = crear.value;
+         if(norep(x)){
+            padre.replaceChild(span, crear);
+            span.innerHTML = x;
+         }
+         
+        }
+      });
+}
+
+function borrar(boton){
+    
+    if(confirm("¿Estás seguro de que quieres borrar el invitado?")){
+        boton.parentElement.remove();
+    }
+        
+}
+    
+function confirmado(caja){
+
+    caja.addEventListener("change", function(){
+        if(caja.checked){
+            caja.parentElement.parentElement.removeAttribute("noconfirmed");
+            caja.parentElement.parentElement.setAttribute("class", "responded");
+        }else{
+            caja.parentElement.parentElement.removeAttribute("responded");
+            caja.parentElement.parentElement.setAttribute("class", "noconfirmed");
+        }
+
+    });
+
+}
+  
+function añadirInvitado(){
+
+   let newLi = document.createElement("li");
+   let nombre = document.createElement("span");
+   let etiqueta = document.createElement("label");
+   let cuadrado = document.createElement("input");
+   let editar = document.createElement("button");
+   let borrar = document.createElement("button");
+
+   newLi.setAttribute("class", "noconfirmed")
+   
+   nombre.innerHTML = document.getElementById("nombre").value;
+   
+   etiqueta.innerHTML = "Confirmed";
+   
+   
+   cuadrado.setAttribute("type","checkbox");
+   etiqueta.appendChild(cuadrado);
+   cuadrado.addEventListener("change", confirmado(cuadrado));
+  
+   editar.innerHTML = "edit";
+   editar.addEventListener("click", function(){
+        editar(nombre, newLi);
+   });
+
+   
+   borrar.innerHTML = "remove";
+   borrar.addEventListener("click", function(){
+    borrar(borrar);
+   });
+
+   newLi.appendChild(nombre);
+   newLi.appendChild(etiqueta);
+   newLi.appendChild(editar);
+   newLi.appendChild(borrar);
+
+   if(norep(nombre)){
+    document.getElementById("invitedList").appendChild(newLi);
+   }
+
+   
+
+}
